8ab13388968fa35de26051ec4e97995da42b46f6
AllanPires95 <allan_sfc@hotmail.com>
2023-01-24 10:44:48 -0300

    versao1

---
+++ src/main/java/org/generation/db_blogpessoal/BlogpessoalApplication.java
@@ -0,0 +1,13 @@
+package org.generation.db_blogpessoal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoalApplication.class, args);
+	}
+
+}
